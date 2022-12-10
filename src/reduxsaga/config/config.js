ed2d43ffa229e4f2242b5d7b43720e7b5f596a18
Aji Setiaji <aji.setiaji@iconpln.co.id>
2022-12-10 08:47:38 +0700

    fix url

--- src/reduxsaga/config/config.js
+++ src/reduxsaga/config/config.js
@@ -1,10 +1,10 @@
 const config = {
-  domain: "https://143.198.200.178:3000/newbackend/v1/api",
+  domain: "https://143.198.200.178/newbackend/v1/api",
   // domain: "https://newbackend.pondoktahfidzquran.my.id/newbackend/v1/api",
   // urlImageCure: "http://localhost:3001/codeid/api/curriculum_reviews/images",
   // urlImageInst: "http://localhost:3001/codeid/api/instructor/images",
   // domainAuth: "http://localhost:3001/codeid/api/auth",
-  urlImage: "https://143.198.200.178:3000/newbackend/v1/api/gambar",
+  urlImage: "https://143.198.200.178/newbackend/v1/api/gambar",
   // urlImage:
   // "https://newbackend.pondoktahfidzquran.my.id/newbackend/v1/api/gambar",
   env: process.env.NODE_ENV || "development",
