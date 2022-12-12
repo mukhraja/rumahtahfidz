ea5417ad4297f19fbdb72b022829cbaa131e4096
Aji Setiaji <aji.setiaji@iconpln.co.id>
2022-12-12 22:45:07 +0700

    perbaikan generate password

--- src/reduxsaga/config/config.js
+++ src/reduxsaga/config/config.js
@@ -1,8 +1,8 @@
 const config = {
-  // domain: "https://pondoktahfidzquran.my.id/newbackend/v1/api",
-  // urlImage: "https://pondoktahfidzquran.my.id/newbackend/v1/api/gambar",
-  domain: "http://localhost:3000/newbackend/v1/api",
-  urlImage: "http://localhost:3000/newbackend/v1/api/gambar",
+  domain: "https://pondoktahfidzquran.my.id/newbackend/v1/api",
+  urlImage: "https://pondoktahfidzquran.my.id/newbackend/v1/api/gambar",
+  // domain: "http://localhost:3000/newbackend/v1/api",
+  // urlImage: "http://localhost:3000/newbackend/v1/api/gambar",
   // domain: "https://newbackend.pondoktahfidzquran.my.id/newbackend/v1/api",
   // urlImageCure: "http://localhost:3001/codeid/api/curriculum_reviews/images",
   // urlImageInst: "http://localhost:3001/codeid/api/instructor/images",
