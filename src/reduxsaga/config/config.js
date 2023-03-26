c8f10d3790d3e40a8f0b1922cfe43d50c064c69e
Aji Setiaji <ajisetiaji101@gmail.com>
2023-03-26 22:12:22 +0700

    fix(config.js): fix urlImageUser endpoint to include '/pondok' in the URL path

--- src/reduxsaga/config/config.js
+++ src/reduxsaga/config/config.js
@@ -6,7 +6,7 @@ const config = {
   domain: "https://backend.pondoktahfidzquran.ac.id/pondok/v1/api",
   urlImage: "https://backend.pondoktahfidzquran.ac.id/pondok/v1/api/gambar",
   urlImageUser:
-    "https://backend.pondoktahfidzquran.ac.id/v1/api/gambar/userdata",
+    "https://backend.pondoktahfidzquran.ac.id/pondok/v1/api/gambar/userdata",
   // domain: "https://newbackend.pondoktahfidzquran.my.id/newbackend/v1/api",
   // urlImageCure: "http://localhost:3001/codeid/api/curriculum_reviews/images",
   // urlImageInst: "http://localhost:3001/codeid/api/instructor/images",
