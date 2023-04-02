7409586bdd1ad6da0cbdeab8fdc681f7d903a7e5
Aji Setiaji <ajisetiaji101@gmail.com>
2023-04-02 17:41:37 +0700

    chore(config.js): switch to production API endpoints and comment out local endpoints

--- src/reduxsaga/config/config.js
+++ src/reduxsaga/config/config.js
@@ -1,11 +1,11 @@
 const config = {
-  domain: "http://localhost:8003/pondok/v1/api",
-  urlImage: "http://localhost:8003/pondok/v1/api/gambar",
-  urlImageUser: "http://localhost:8003/pondok/v1/api/gambar/userdata",
-  // domain: "https://backend.pondoktahfidzquran.ac.id/pondok/v1/api",
-  // urlImage: "https://backend.pondoktahfidzquran.ac.id/pondok/v1/api/gambar",
-  // urlImageUser:
-  //   "https://backend.pondoktahfidzquran.ac.id/pondok/v1/api/gambar/userdata",
+  // domain: "http://localhost:8003/pondok/v1/api",
+  // urlImage: "http://localhost:8003/pondok/v1/api/gambar",
+  // urlImageUser: "http://localhost:8003/pondok/v1/api/gambar/userdata",
+  domain: "https://backend.pondoktahfidzquran.ac.id/pondok/v1/api",
+  urlImage: "https://backend.pondoktahfidzquran.ac.id/pondok/v1/api/gambar",
+  urlImageUser:
+    "https://backend.pondoktahfidzquran.ac.id/pondok/v1/api/gambar/userdata",
   // domain: "https://newbackend.pondoktahfidzquran.my.id/newbackend/v1/api",
   // urlImageCure: "http://localhost:3001/codeid/api/curriculum_reviews/images",
   // urlImageInst: "http://localhost:3001/codeid/api/instructor/images",
